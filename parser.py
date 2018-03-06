e1fbee57069611dcaadafee5a4b528519efdbf92
Eran <eran505@gmail.com>
2018-03-06 10:09:52 +0200

    03/06 10:09:52

--- parser.py
+++ parser.py
@@ -4,12 +4,25 @@ import pandas as pd
 
 class Node(object):
 
-    def __init__(self):
+    def __init__(self,id_node,id_father,n_author,data_i,text):
         self.descendants=None
-        self.data=None
-        self.name =''
-        self.prefix=''
+        self.data=data_i
+        self.i_id =id_node
+        self.author=n_author
+        self.father = id_father
+        self.txt_data = text
 
+gTree = {}
+gAuthor = {}
+def set_gTree(key,val):
+    global gTree
+    gTree[key]=val
+
+def set_gAuthor(key,val):
+    global gAuthor
+    if key not in gAuthor:
+        gAuthor[key]=[]
+    gAuthor[key].append(val)
 
 
 
@@ -28,13 +41,15 @@ def data_handler(string_data,d):
     #print "data = {}".format(string_data)
     data = json.loads(string_data)
     name_subreddit = data['subreddit']
-    if name_subreddit in d:
-        d[name_subreddit]+=1
-    else:
-        d[name_subreddit] = 1
-        print d
+    id = data['link_id']
+    permalink = data['permalink'] #need to contain the link id
+    counter = 0
+    if str(id).__contains__('7tu0ft'):
+        tree_construct(data)
+    return False
 
-def main_fun(file_path):
+
+def main_fun(file_path): #7tu0ft
     d={}
     ctr=0
     print ""
@@ -42,12 +57,38 @@ def main_fun(file_path):
         raise "[Error] cant find the json file --> {}".format(file_path)
     with open(file_path) as f:
         for line in f:
-            data_handler(line,d)
-            print ctr
-            ctr+=1
-    print d
+            if data_handler(line,d):
+                ctr += 1
+    make_tree()
+    print gTree
+    print gAuthor
 
+def tree_construct(json_data):
+    parent_id = json_data['parent_id']
+    link_data = json_data['permalink']
+    author_name = json_data['author']
+    id_link = json_data['link_id']
+    id = json_data['id']
+    text_data = json_data['body']
+    node = Node(id,parent_id,author_name,json_data,text_data)
+    if id in gTree:
+        print "[Error] in {}".format(json_data)
+    else:
+        set_gTree(id, node)
+
+def make_tree():
+    for key in gTree:
+        parent = gTree[key].father[3:]
+        if parent in gTree:
+            node_j = gTree[parent]
+            if node_j.descendants is None:
+                node_j.descendants={}
+                node_j.descendants[key]=gTree[key]
+            else:
+                node_j.descendants[key] = gTree[key]
+        author_name = gTree[key].author
+        set_gAuthor(author_name,key)
 
 if __name__ == "__main__":
-    path = '/home/eran/Downloads/RC_2018-01-23'
+    path = '/home/eran/Downloads/RC_2018-01-29'
     main_fun(path)
